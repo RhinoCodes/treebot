226cbe00c95a4ec3cb9f73932edcb3fd869b5b25
SuperMeteorite <51250021+SuperMeteorite@users.noreply.github.com>
2019-11-23 18:00:03 -0600

    Update treebot.py
    still isn't working, check back later!
    Added Word Loop

--- treebot.py
+++ treebot.py
@@ -1,3 +1,6 @@
 import requests
 
-requests.get('https://ecosia.org/search?q=GitHub')
+words = request.get('https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json').text
+words = eval(words)
+for k in words.keys():
+  requests.get(f'https://ecosia.org/search?q={k}')

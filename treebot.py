9b2babf91aa990332f0c9abda5d9db53e3155fd9
SuperMeteorite <51250021+SuperMeteorite@users.noreply.github.com>
2019-11-23 18:01:39 -0600

    Update treebot.py
    Still isn't working! Check back later
    Fixed Getting all 479K words

--- treebot.py
+++ treebot.py
@@ -1,6 +1,6 @@
 import requests
 
-words = request.get('https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json').text
+words = requests.get('https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json').text
 words = eval(words)
 for k in words.keys():
   requests.get(f'https://ecosia.org/search?q={k}')

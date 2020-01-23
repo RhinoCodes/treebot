8194cdaa5572f94e1ee36241b06c8e2fd86d305b
RhinoCodes <51250021+RhinoCodes@users.noreply.github.com>
2020-01-23 16:52:53 -0600

    Delete treebot.py

--- treebot.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-words = requests.get('https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json').text
-words = eval(words)
-for k in words.keys():
-  requests.get(f'https://ecosia.org/search?q={k}')
